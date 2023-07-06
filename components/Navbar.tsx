62d7aef2e4ea87b727823006976982c7afa56d52
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-07-06 09:17:47 +0700

    Created LeftSide Bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         >
           <Image src={logo} alt="logo" priority className="w-14" />
         </motion.div>
-        {/* Desktop */}
+        {/* Desktop Menu */}
         <div className="hidden mdl:inline-flex items-center gap-7">
           <ul className="flex text-[16px] gap-7">
             <Link
@@ -91,11 +91,23 @@ const Navbar = () => {
             </motion.button>
           </a>
         </div>
-        {/* Mobile - Custom Menu Icon */}
-        <div className="w-6 h-5 flex flex-col justify-between items-center md:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group">
-          <span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
-          <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
-          <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
+        {/* Mobile Menu Icon */}
+        <div
+          className="w-6 h-5 flex flex-col justify-between items-center md:hidden text-4xl text-textGreen
+        cursor-pointer overflow-hidden group"
+        >
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all
+          ease-in-out duration-300"
+          ></span>
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0
+          transition-all ease-in-out duration-300"
+          ></span>
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3
+          transition-all ease-in-out duration-300"
+          ></span>
         </div>
       </div>
     </div>
