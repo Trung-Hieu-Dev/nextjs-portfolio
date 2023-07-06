0373278a2e1deb53a9e720731309de2fca2a0140
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-07-06 09:57:16 +0700

    Animate Leftside and Rightside with frammer-motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import LeftSide from "@/components/LeftSide";
-import Navbar from "@/components/Navbar";
 import Head from "next/head";
+import { motion } from "framer-motion";
+
+import Navbar from "@/components/Navbar";
 import RightSide from "@/components/RightSide";
+import LeftSide from "@/components/LeftSide";
 
 export default function Home() {
   return (
@@ -16,13 +18,23 @@ export default function Home() {
         <Navbar />
         {/* Main Content   */}
         <div className="w-full h-[88vh] xl:flex items-center justify-between gap-20">
-          <div className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
+          >
             <LeftSide />
-          </div>
+          </motion.div>
           <div className="h-[88vh] mx-auto p-4">Middle</div>
-          <div className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          >
             <RightSide />
-          </div>
+          </motion.div>
         </div>
       </main>
     </>
