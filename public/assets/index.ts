d49912a6600e4aa5ab9af8fb9e5da4932bbc5370
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-07-10 10:05:37 +0700

    Checking code before deploying. Added one more personal project

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -2,5 +2,12 @@ import logo from "./images/logo.png";
 import profileImg from "./images/hieu.jpg";
 import vuejsMusicPlayerImg from "./images/vuejs_music_player.png";
 import nextjsBlogImg from "./images/nextjs_blog.png";
+import nextjsPortfolioImg from "./images/nextjs_portfolio.png";
 
-export { logo, profileImg, vuejsMusicPlayerImg, nextjsBlogImg };
+export {
+  logo,
+  profileImg,
+  vuejsMusicPlayerImg,
+  nextjsBlogImg,
+  nextjsPortfolioImg,
+};
